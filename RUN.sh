42c52e30340bc3ae6f2fe6f45eda4f9b710b210e
Juergen Weigert <jnweiger@gmail.com>
2023-12-16 23:58:00 +0100

    Updated RUN.sh to have (almost?) all dependencies. It might even run inside docker... (but that is not recommended)

--- RUN.sh
+++ RUN.sh
@@ -1,19 +1,77 @@
-#!/bin/sh
+#! /bin/bash
 #
 # Generate the inofficial fablabnbg build of visicut.
 # We basically pull a special version of liblasercut and build that wit the current master.
 #
 # Requires:
 #   appimagecraft from https://appimage.github.io/appimagecraft/
+#
+# Tested in: docker run --rm -ti -v $PWD:/mnt ubuntu:22.04 bash; /mnt/RUN.sh
+#    -> follow the instructions... and all deliverables should (somewhat unreliably) build.
+#       you may need to manually repeat some of the distribute.sh calls.
 
 visicut_tag=master
 liblasercut_tag=master
 
-if [ -z "$(which appimagecraft)" ]; then
-  echo "ERROR: appimagecraft not installed! Download"
-  echo "        https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage"
-  echo "        and install it in your PATH as appimagecraft"
+depapt=
+deperr=
+
+checkcmd() {
+if [ -z "$(which $1)" ]; then
+  test -n "$2" && deperr="$deperr
+ERROR: $2
+"
+  test -z "$2$3" && depapt="$depapt $1"
+  test -n "$3"   && depapt="$depapt $3"
+fi
+}
+
+
+checkcmd wget
+checkcmd git
+checkcmd make
+checkcmd zip
+checkcmd unzip
+checkcmd checkinstall
+checkcmd makensis "" nsis
+checkcmd mvn "" maven
+checkcmd rsvg-convert "" librsvg2-bin
+checkcmd fusermount "" fuse
+checkcmd appimagecraft "appimagecraft not installed! Try:
+    sudo wget -O  /usr/local/bin/appimagecraft https://github.com/TheAssassin/appimagecraft/releases/download/continuous/appimagecraft-x86_64.AppImage
+    sudo chmod +x /usr/local/bin/appimagecraft"
+
+
+if [ -n "$depapt$deperr" ]; then
+  if [ -n "$depapt" ]; then
+    echo "ERROR: missing packages. Try:"
+    echo "    sudo apt install$depapt"
+  fi
+  test -n "$deperr" && echo "$deperr"
+  echo ""
+  echo "... then try again..."
   exit 1
+fi
+
+if [ -z "$(git remote -v 2>&1 | grep VisiCut)" ]; then
+  echo "ERROR: current directory is not a Visicut checkout. Try either"
+  echo "  change directory to your checkout, if you have one,"
+  echo "  or run e.g:"
+  echo " 	git clone --recursive https://github.com/fablabnbg/VisiCut"
+  echo " 	cd VisiCut"
+  echo "  then retry:"
+  echo "        bash $(readlink -f $0)"
+  echo ""
+  exit 1
+fi
+
+if [ -e /.dockerenv ]; then
+  # Seen in https://github.com/AppImage/AppImageKit/issues/1027#issuecomment-1028232809
+  export APPIMAGE_EXTRACT_AND_RUN=1
+  # The mkdir calls in distribute.sh linux-checkinstall silently fail in docker. Do them ahead of time ... that helps.
+  mkdir -p /usr/share/visicut/{inkscape_extension,illustrator_script}
+  mkdir -p /usr/share/pixmaps
+  cp -r distribute/files/* /usr/share/visicut/	# so that all directories get created...
 fi
 
 git checkout $visicut_tag
@@ -42,4 +100,9 @@ env NO_BUILD=1 ./distribute/distribute.sh macos-bundle
 env NO_BUILD=1 ./distribute/distribute.sh linux-checkinstall
 env NO_BUILD=1 ./distribute/distribute.sh linux-appimage
 ls -la VisiCut-*.exe VisiCutMac-*.zip visicut*.deb VisiCut*.AppImage
+
+echo ""
+echo "during development, you can use:"
+jar="$(echo target/visicut-*-full.jar)"
+echo "    make; java -Xms256m -Xmx2048m -jar $jar"
 
